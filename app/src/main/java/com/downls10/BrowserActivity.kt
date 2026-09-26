2267165d9525900aae547eb93df79c35452c11dc
Edward33-slim <Edward33@proton.me>
2026-09-26 12:48:21 +0300

    Fix Custom Tab layout import

--- app/src/main/java/com/downls10/BrowserActivity.kt
+++ app/src/main/java/com/downls10/BrowserActivity.kt
@@ -41,6 +41,7 @@ import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
+import android.widget.RelativeLayout
 import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
