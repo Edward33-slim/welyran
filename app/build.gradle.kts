381bab098b8798c772c4e2a52d77c1707195aba3
Edward33-slim <Edward33@proton.me>
2026-09-26 13:44:30 +0300

    Bump DownLS10 to 6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.downls10"
         minSdk = 24
         targetSdk = 34
-        versionCode = 59
-        versionName = "5.9"
+        versionCode = 60
+        versionName = "6.0"
     }
 
     signingConfigs {
